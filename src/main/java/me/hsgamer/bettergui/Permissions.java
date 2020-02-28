6430f77a57d595e74b670c154a2765c3320e6059
HSGamer <huynhqtienvtag@gmail.com>
2020-02-28 18:50:21 +0700

    [ci-skip] reformat

--- src/main/java/me/hsgamer/bettergui/Permissions.java
+++ src/main/java/me/hsgamer/bettergui/Permissions.java
@@ -9,7 +9,7 @@ public class Permissions {
 
   private static final String PREFIX = getInstance().getName()
       .toLowerCase();
-  
+
   public static final Permission OPEN_MENU = createPermission(PREFIX + ".openmenu", null,
       PermissionDefault.OP);
   public static final Permission RELOAD = createPermission(PREFIX + ".reload", null,
