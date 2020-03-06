8fff44e45b1b98b86234d4d21c72ed3786c45c19
HSGamer <huynhqtienvtag@gmail.com>
2020-03-06 11:22:02 +0700

    add some information

--- src/main/java/me/hsgamer/bettergui/command/MainCommand.java
+++ src/main/java/me/hsgamer/bettergui/command/MainCommand.java
@@ -1,12 +1,13 @@
 package me.hsgamer.bettergui.command;
 
+import static me.hsgamer.bettergui.util.CommonUtils.sendMessage;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import me.hsgamer.bettergui.BetterGUI;
 import me.hsgamer.bettergui.Permissions;
 import me.hsgamer.bettergui.config.impl.MessageConfig.DefaultMessage;
 import me.hsgamer.bettergui.manager.CommandManager;
-import me.hsgamer.bettergui.util.CommonUtils;
 import me.hsgamer.bettergui.util.TestCase;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
@@ -17,22 +18,27 @@ public class MainCommand extends BukkitCommand {
       .setPredicate(sender -> sender.hasPermission(Permissions.HELP))
       .setSuccessConsumer(sender -> {
         CommandManager manager = BetterGUI.getInstance().getCommandManager();
-        CommonUtils.sendMessage(sender, "");
-        CommonUtils.sendMessage(sender, "&b&lCommand: ");
+        sendMessage(sender, "");
+        sendMessage(sender, "&e&lAuthor: &f" + Arrays
+            .toString(BetterGUI.getInstance().getDescription().getAuthors().toArray()));
+        sendMessage(sender, "&e&lVersion: &f" + BetterGUI.getInstance().getDescription().getVersion());
+        sendMessage(sender, "&9&lDiscord: &fhttps://discord.gg/8mJJMqH");
+        sendMessage(sender, "");
+        sendMessage(sender, "&b&lCommand: ");
         for (BukkitCommand command : manager.getRegistered().values()) {
-          CommonUtils.sendMessage(sender,
+          sendMessage(sender,
               "  &6" + command.getUsage() + ": &f" + command.getDescription());
-          CommonUtils.sendMessage(sender,
+          sendMessage(sender,
               "    &cAlias: " + Arrays.toString(command.getAliases().toArray()));
         }
-        CommonUtils.sendMessage(sender, "");
-        CommonUtils.sendMessage(sender, "&b&lMenu Command: ");
+        sendMessage(sender, "");
+        sendMessage(sender, "&b&lMenu Command: ");
         for (BukkitCommand command : manager.getRegisteredMenuCommand().values()) {
-          CommonUtils.sendMessage(sender, "  &6" + command.getUsage());
+          sendMessage(sender, "  &6" + command.getUsage());
         }
-        CommonUtils.sendMessage(sender, "");
+        sendMessage(sender, "");
       })
-      .setFailConsumer(sender -> CommonUtils.sendMessage(sender,
+      .setFailConsumer(sender -> sendMessage(sender,
           BetterGUI.getInstance().getMessageConfig().get(DefaultMessage.NO_PERMISSION)));
 
   public MainCommand(String name) {
