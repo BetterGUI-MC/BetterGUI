015ed60600a75c6d56b5e482ad6b263a56811958
HSGamer <huynhqtienvtag@gmail.com>
2020-04-16 13:40:32 +0700

    support Menu in BackCommand

--- src/main/java/me/hsgamer/bettergui/object/command/BackCommand.java
+++ src/main/java/me/hsgamer/bettergui/object/command/BackCommand.java
@@ -20,13 +20,20 @@ public class BackCommand extends Command {
   public void addToTaskChain(Player player, TaskChain<?> taskChain) {
     Object object = getVariableManager().getParent();
     Runnable runnable = player::closeInventory;
+
+    Optional<Menu<?>> parentMenu = Optional.empty();
     if (object instanceof Icon) {
-      Optional<Menu<?>> parentMenu = ((Icon) object).getMenu().getParentMenu(player);
-      if (parentMenu.isPresent()) {
-        runnable = () -> parentMenu.get().createInventory(player, new String[0],
-            player.hasPermission(Permissions.OPEN_MENU_BYPASS));
-      }
+      parentMenu = ((Icon) object).getMenu().getParentMenu(player);
+    } else if (object instanceof Menu) {
+      parentMenu = ((Menu<?>) object).getParentMenu(player);
     }
+
+    if (parentMenu.isPresent()) {
+      Menu<?> finalParentMenu = parentMenu.get();
+      runnable = () -> finalParentMenu.createInventory(player, new String[0],
+          player.hasPermission(Permissions.OPEN_MENU_BYPASS));
+    }
+
     Runnable finalRunnable = runnable;
     taskChain.sync(() -> getInstance().getServer().getScheduler().scheduleSyncDelayedTask(
         getInstance(), finalRunnable));
