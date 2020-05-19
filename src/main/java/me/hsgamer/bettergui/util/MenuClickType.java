f08b2b5d1a3ddc91e35267f9f0e2844db88d5236
HSGamer <huynhqtienvtag@gmail.com>
2020-05-19 21:43:29 +0700

    hotbar slots

--- src/main/java/me/hsgamer/bettergui/util/MenuClickType.java
+++ src/main/java/me/hsgamer/bettergui/util/MenuClickType.java
@@ -21,19 +21,25 @@ public enum MenuClickType {
   UNKNOWN(ClickType.UNKNOWN),
 
   // Numbered number key
-  NUMBER_KEY_1(ClickType.NUMBER_KEY),
-  NUMBER_KEY_2(ClickType.NUMBER_KEY),
-  NUMBER_KEY_3(ClickType.NUMBER_KEY),
-  NUMBER_KEY_4(ClickType.NUMBER_KEY),
-  NUMBER_KEY_5(ClickType.NUMBER_KEY),
-  NUMBER_KEY_6(ClickType.NUMBER_KEY),
-  NUMBER_KEY_7(ClickType.NUMBER_KEY),
-  NUMBER_KEY_8(ClickType.NUMBER_KEY),
-  NUMBER_KEY_9(ClickType.NUMBER_KEY);
+  NUMBER_KEY_1(ClickType.NUMBER_KEY, 0),
+  NUMBER_KEY_2(ClickType.NUMBER_KEY, 1),
+  NUMBER_KEY_3(ClickType.NUMBER_KEY, 2),
+  NUMBER_KEY_4(ClickType.NUMBER_KEY, 3),
+  NUMBER_KEY_5(ClickType.NUMBER_KEY, 4),
+  NUMBER_KEY_6(ClickType.NUMBER_KEY, 5),
+  NUMBER_KEY_7(ClickType.NUMBER_KEY, 6),
+  NUMBER_KEY_8(ClickType.NUMBER_KEY, 7),
+  NUMBER_KEY_9(ClickType.NUMBER_KEY, 8);
   private final ClickType clickType;
+  private final int hotbarSlot;
+
+  MenuClickType(ClickType clickType, int hotbarSlot) {
+    this.clickType = clickType;
+    this.hotbarSlot = hotbarSlot;
+  }
 
   MenuClickType(ClickType clickType) {
-    this.clickType = clickType;
+    this(clickType, -1);
   }
 
   public static MenuClickType fromEvent(InventoryClickEvent event) {
@@ -48,5 +54,10 @@ public enum MenuClickType {
   @SuppressWarnings("unused")
   public ClickType getBukkitClickType() {
     return clickType;
+  }
+
+  @SuppressWarnings("unused")
+  public int getHotbarSlot() {
+    return hotbarSlot;
   }
 }
