4b2bbe5296e721df27caefbfa86bdb2ef9ea29dd
hsgamer <huynhqtienvtag@gmail.com>
2020-12-05 11:17:37 +0700

    AddonManager

---
+++ src/main/java/me/hsgamer/bettergui/command/GetAddonsCommand.java
@@ -0,0 +1,41 @@
+package me.hsgamer.bettergui.command;
+
+import me.hsgamer.bettergui.BetterGUI;
+import me.hsgamer.bettergui.Permissions;
+import me.hsgamer.bettergui.config.MessageConfig;
+import me.hsgamer.bettergui.manager.BetterGUIAddonManager.Setting;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
+
+import java.util.Arrays;
+
+import static me.hsgamer.hscore.bukkit.utils.MessageUtils.sendMessage;
+
+public final class GetAddonsCommand extends BukkitCommand {
+
+  public GetAddonsCommand() {
+    super("addons", "Get the loaded addons", "/addons",
+      Arrays.asList("menuaddons", "getmenuaddons"));
+  }
+
+  @Override
+  public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+    if (!sender.hasPermission(Permissions.ADDONS)) {
+      sendMessage(sender, MessageConfig.NO_PERMISSION.getValue());
+      return false;
+    }
+
+    boolean shortMessage = args.length > 0 && args[0].equalsIgnoreCase("short");
+
+    sendMessage(sender, "&b&lLoaded Addons:");
+    BetterGUI.getInstance().getAddonManager().getLoadedAddons().forEach((name, addon) -> {
+      sendMessage(sender, "  &f- &a" + name);
+      if (!shortMessage) {
+        sendMessage(sender, "    &eVersion: &f" + addon.getDescription().getVersion());
+        sendMessage(sender, "    &eAuthors: &f" + Setting.AUTHORS.get(addon));
+        sendMessage(sender, "    &eDescription: &f" + Setting.DESCRIPTION.get(addon));
+      }
+    });
+    return true;
+  }
+}
