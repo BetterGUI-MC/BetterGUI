7abec2ccee412e8defaac55e954faecd444e1d3c
hsgamer <huynhqtienvtag@gmail.com>
2021-03-28 20:25:46 +0700

    use Bukkit's Permission

--- src/main/java/me/hsgamer/bettergui/Permissions.java
+++ src/main/java/me/hsgamer/bettergui/Permissions.java
@@ -4,19 +4,18 @@ import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
 import static me.hsgamer.bettergui.BetterGUI.getInstance;
-import static me.hsgamer.hscore.bukkit.utils.PermissionUtils.createPermission;
 
 public final class Permissions {
 
   public static final String PREFIX = getInstance().getName().toLowerCase();
 
-  public static final Permission OPEN_MENU = createPermission(PREFIX + ".openmenu", null, PermissionDefault.OP);
-  public static final Permission RELOAD = createPermission(PREFIX + ".reload", null, PermissionDefault.OP);
-  public static final Permission ADDONS = createPermission(PREFIX + ".addons", null, PermissionDefault.OP);
-  public static final Permission HELP = createPermission(PREFIX + ".help", null, PermissionDefault.OP);
-  public static final Permission VARIABLE = createPermission(PREFIX + ".variable", null, PermissionDefault.OP);
-  public static final Permission OPEN_MENU_BYPASS = createPermission(PREFIX + ".openmenu.bypass", null, PermissionDefault.OP);
-  public static final Permission ADDON_DOWNLOADER = createPermission(PREFIX + ".addons.downloader", null, PermissionDefault.OP);
+  public static final Permission OPEN_MENU = new Permission(PREFIX + ".openmenu", PermissionDefault.OP);
+  public static final Permission RELOAD = new Permission(PREFIX + ".reload", PermissionDefault.OP);
+  public static final Permission ADDONS = new Permission(PREFIX + ".addons", PermissionDefault.OP);
+  public static final Permission HELP = new Permission(PREFIX + ".help", PermissionDefault.OP);
+  public static final Permission VARIABLE = new Permission(PREFIX + ".variable", PermissionDefault.OP);
+  public static final Permission OPEN_MENU_BYPASS = new Permission(PREFIX + ".openmenu.bypass", PermissionDefault.OP);
+  public static final Permission ADDON_DOWNLOADER = new Permission(PREFIX + ".addons.downloader", PermissionDefault.OP);
 
   private Permissions() {
 
