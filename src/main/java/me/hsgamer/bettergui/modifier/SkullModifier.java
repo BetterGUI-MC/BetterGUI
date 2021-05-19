c2a9c5594ed2e147aee6136f85ab4173a9bd8855
hsgamer <huynhqtienvtag@gmail.com>
2021-05-19 21:27:12 +0700

    support Paper's PlayerProfile for loading player skulls async

--- src/main/java/me/hsgamer/bettergui/modifier/SkullModifier.java
+++ src/main/java/me/hsgamer/bettergui/modifier/SkullModifier.java
@@ -1,11 +1,14 @@
 package me.hsgamer.bettergui.modifier;
 
 import com.cryptomorin.xseries.SkullUtils;
+import com.cryptomorin.xseries.XMaterial;
 import me.hsgamer.bettergui.config.MainConfig;
 import me.hsgamer.hscore.bukkit.item.ItemMetaModifier;
 import me.hsgamer.hscore.bukkit.utils.BukkitUtils;
+import me.hsgamer.hscore.common.Validate;
 import me.hsgamer.hscore.common.interfaces.StringReplacer;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 
@@ -15,6 +18,12 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 public class SkullModifier extends ItemMetaModifier {
+  private static final boolean IS_PAPER;
+
+  static {
+    IS_PAPER = Validate.isClassLoaded("com.destroystokyo.paper.PaperConfig");
+  }
+
   private String skullString = "";
 
   @Override
@@ -29,7 +38,15 @@ public class SkullModifier extends ItemMetaModifier {
       if (Boolean.TRUE.equals(MainConfig.LOAD_PLAYER_SKULL_ASYNC.getValue()) && BukkitUtils.isUsername(value)) {
         CompletableFuture<OfflinePlayer> completableFuture = BukkitUtils.getOfflinePlayerAsync(value);
         if (completableFuture.isDone()) {
-          return SkullUtils.applySkin(meta, completableFuture.join());
+          OfflinePlayer offlinePlayer = completableFuture.join();
+          if (IS_PAPER && XMaterial.getVersion() >= 12 && offlinePlayer.isOnline()) {
+            Player player = offlinePlayer.getPlayer();
+            if (player.getPlayerProfile().hasTextures()) {
+              ((SkullMeta) meta).setPlayerProfile(player.getPlayerProfile());
+            }
+          } else {
+            return SkullUtils.applySkin(meta, offlinePlayer);
+          }
         }
       } else {
         return SkullUtils.applySkin(meta, value);
