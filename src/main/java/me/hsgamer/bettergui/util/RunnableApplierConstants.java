09126b627cffe8d7a9e9c74eaf673b978344539f
HSGamer <huynhqtienvtag@gmail.com>
2022-07-24 11:01:05 +0700

    RunnableApplierConstants

---
+++ src/main/java/me/hsgamer/bettergui/util/RunnableApplierConstants.java
@@ -0,0 +1,10 @@
+package me.hsgamer.bettergui.util;
+
+/**
+ * Constants for {@link me.hsgamer.bettergui.api.RunnableApplier}
+ */
+public final class RunnableApplierConstants {
+  private RunnableApplierConstants() {
+    // EMPTY
+  }
+}
