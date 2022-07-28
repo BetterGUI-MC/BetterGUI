a416d7c9da4e9621037a1f56adb2564d763487fd
HSGamer <huynhqtienvtag@gmail.com>
2022-07-28 16:09:44 +0700

    unused

--- src/main/java/me/hsgamer/bettergui/util/ProcessApplierConstants.java
+++ src/main/java/me/hsgamer/bettergui/util/ProcessApplierConstants.java
@@ -6,7 +6,6 @@ import me.hsgamer.bettergui.api.process.ProcessApplier;
  * Constants for {@link ProcessApplier}
  */
 public final class ProcessApplierConstants {
-  public static final int REQUIREMENT_SETTING_STAGE = 0;
   public static final int REQUIREMENT_ACTION_STAGE = 9;
   public static final int ACTION_STAGE = 99;
 
