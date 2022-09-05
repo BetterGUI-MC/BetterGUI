ef28e3c3a06bb35df47c48eed9f3779e33a6e9b8
HSGamer <huynhqtienvtag@gmail.com>
2022-09-05 07:57:48 +0700

    catch exception on PlaceholderAPI
    let just throw exception as log instead of halting the thread because of it

--- src/main/java/me/hsgamer/bettergui/papi/ExtraPlaceholderExpansion.java
+++ src/main/java/me/hsgamer/bettergui/papi/ExtraPlaceholderExpansion.java
@@ -2,6 +2,7 @@ package me.hsgamer.bettergui.papi;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import me.hsgamer.bettergui.BetterGUI;
 import me.hsgamer.hscore.variable.ExternalStringReplacer;
 import me.hsgamer.hscore.variable.VariableManager;
 import org.bukkit.Bukkit;
@@ -11,6 +12,7 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.UUID;
+import java.util.logging.Level;
 
 import static me.hsgamer.bettergui.BetterGUI.getInstance;
 
@@ -32,12 +34,22 @@ public class ExtraPlaceholderExpansion extends PlaceholderExpansion {
       VariableManager.addExternalReplacer(new ExternalStringReplacer() {
         @Override
         public String replace(String original, UUID uuid) {
-          return PlaceholderAPI.setPlaceholders(Bukkit.getOfflinePlayer(uuid), original);
+          try {
+            return PlaceholderAPI.setPlaceholders(Bukkit.getOfflinePlayer(uuid), original);
+          } catch (Throwable throwable) {
+            BetterGUI.getInstance().getLogger().log(Level.WARNING, "Error while replacing placeholders", throwable);
+            return original;
+          }
         }
 
         @Override
         public boolean canBeReplaced(String original) {
-          return PlaceholderAPI.containsPlaceholders(original);
+          try {
+            return PlaceholderAPI.containsPlaceholders(original);
+          } catch (Throwable throwable) {
+            BetterGUI.getInstance().getLogger().log(Level.WARNING, "Error while checking PlaceholderAPI placeholders", throwable);
+            return false;
+          }
         }
       });
     }
