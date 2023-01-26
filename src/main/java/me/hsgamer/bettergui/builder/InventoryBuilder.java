b15ed9679fb512291b3e28bb475a383f693c8e49
HSGamer <huynhqtienvtag@gmail.com>
2023-01-26 20:35:24 +0700

    update InventoryBuilder

--- src/main/java/me/hsgamer/bettergui/builder/InventoryBuilder.java
+++ src/main/java/me/hsgamer/bettergui/builder/InventoryBuilder.java
@@ -2,9 +2,9 @@ package me.hsgamer.bettergui.builder;
 
 import me.hsgamer.bettergui.api.menu.Menu;
 import me.hsgamer.hscore.builder.Builder;
-import me.hsgamer.hscore.bukkit.gui.GUIDisplay;
-import me.hsgamer.hscore.bukkit.gui.GUIHolder;
-import me.hsgamer.hscore.bukkit.gui.GUIUtils;
+import me.hsgamer.hscore.bukkit.gui.BukkitGUIDisplay;
+import me.hsgamer.hscore.bukkit.gui.BukkitGUIHolder;
+import me.hsgamer.hscore.bukkit.gui.BukkitGUIUtils;
 import me.hsgamer.hscore.common.Pair;
 import org.bukkit.Bukkit;
 import org.bukkit.event.inventory.InventoryType;
@@ -17,7 +17,7 @@ import java.util.function.BiFunction;
 /**
  * A builder to get the creator to build the {@link Inventory} for any {@link me.hsgamer.bettergui.menu.BaseInventoryMenu} implementation
  */
-public class InventoryBuilder extends Builder<Pair<Menu, Map<String, Object>>, BiFunction<GUIDisplay, UUID, Inventory>> {
+public class InventoryBuilder extends Builder<Pair<Menu, Map<String, Object>>, BiFunction<BukkitGUIDisplay, UUID, Inventory>> {
   /**
    * The singleton instance
    */
@@ -25,12 +25,12 @@ public class InventoryBuilder extends Builder<Pair<Menu, Map<String, Object>>, B
 
   private InventoryBuilder() {
     register(() -> (display, uuid) -> {
-      GUIHolder holder = display.getHolder();
+      BukkitGUIHolder holder = display.getHolder();
       InventoryType type = holder.getInventoryType();
       int size = holder.getSize(uuid);
       String title = holder.getTitle(uuid);
       return type == InventoryType.CHEST && size > 0
-        ? Bukkit.createInventory(display, GUIUtils.normalizeToChestSize(size), title)
+        ? Bukkit.createInventory(display, BukkitGUIUtils.normalizeToChestSize(size), title)
         : Bukkit.createInventory(display, type, title);
     }, "default");
   }
