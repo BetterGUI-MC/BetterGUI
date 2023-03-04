59223a40e5859d09d2be81ca5033afea652753a9
HSGamer <huynhqtienvtag@gmail.com>
2023-03-04 23:39:21 +0700

    generate plugin.yml from Maven settings

---
+++ src/main/java-templates/me/hsgamer/bettergui/PluginBuild.java
@@ -0,0 +1,13 @@
+package me.hsgamer.bettergui;
+
+final class PluginBuild {
+  static final String NAME = "${project.name}";
+  static final String VERSION = "${project.version}";
+  static final String DESCRIPTION = "${project.description}";
+  static final String AUTHOR = "HSGamer";
+  static final String WEBSITE = "https://github.com/BetterGUI-MC";
+
+  private PluginBuild() {
+    throw new IllegalStateException("Utility class");
+  }
+}
