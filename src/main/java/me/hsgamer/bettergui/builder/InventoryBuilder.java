e1d3194155772eb028cd8d285d5e89e471dfef93
hsgamer <huynhqtienvtag@gmail.com>
2025-12-05 01:53:34 +0700

    provide BaseInventoryMenu in InventoryBuilder

--- src/main/java/me/hsgamer/bettergui/builder/InventoryBuilder.java
+++ src/main/java/me/hsgamer/bettergui/builder/InventoryBuilder.java
@@ -1,6 +1,6 @@
 package me.hsgamer.bettergui.builder;
 
-import me.hsgamer.bettergui.api.menu.Menu;
+import me.hsgamer.bettergui.menu.BaseInventoryMenu;
 import me.hsgamer.hscore.builder.Builder;
 import me.hsgamer.hscore.common.Pair;
 import org.bukkit.inventory.Inventory;
@@ -13,7 +13,7 @@ import java.util.function.BiFunction;
 /**
  * A builder to get the creator to build the {@link Inventory} for any {@link me.hsgamer.bettergui.menu.BaseInventoryMenu} implementation
  */
-public class InventoryBuilder extends Builder<Pair<Menu, Map<String, Object>>, BiFunction<UUID, InventoryHolder, Inventory>> {
+public class InventoryBuilder extends Builder<Pair<BaseInventoryMenu<?>, Map<String, Object>>, BiFunction<UUID, InventoryHolder, Inventory>> {
   /**
    * The singleton instance
    */
