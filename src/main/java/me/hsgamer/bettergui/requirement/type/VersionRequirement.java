fa89f771c984b20a6b87db99911b6c08aa98155a
hsgamer <huynhqtienvtag@gmail.com>
2026-05-01 10:06:48 +0700

    backport: support full version pattern in VersionRequirement

--- src/main/java/me/hsgamer/bettergui/requirement/type/VersionRequirement.java
+++ src/main/java/me/hsgamer/bettergui/requirement/type/VersionRequirement.java
@@ -10,26 +10,77 @@ import me.hsgamer.hscore.bukkit.utils.VersionUtils;
 import me.hsgamer.hscore.common.Validate;
 
 import java.math.BigDecimal;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class VersionRequirement extends BaseRequirement<Integer> {
+public class VersionRequirement extends BaseRequirement<VersionRequirement.Version> {
+  private static final Pattern VERSION_PATTERN = Pattern.compile("(\\d+)\\.(\\d+)(\\.(\\d+))?");
+
   public VersionRequirement(RequirementBuilder.Input input) {
     super(input);
   }
 
   @Override
-  protected Integer convert(Object value, UUID uuid) {
+  protected Version convert(Object value, UUID uuid) {
     String replaced = StringReplacerApplier.replace(String.valueOf(value).trim(), uuid, this);
-    return Validate.getNumber(replaced)
-      .map(BigDecimal::intValue)
-      .orElseGet(() -> {
-        MessageUtils.sendMessage(uuid, BetterGUI.getInstance().get(MessageConfig.class).getInvalidNumber(replaced));
-        return 0;
-      });
+    Matcher versionMatcher = VERSION_PATTERN.matcher(replaced);
+    if (versionMatcher.find()) {
+      int major = Integer.parseInt(versionMatcher.group(1));
+      int minor = Integer.parseInt(versionMatcher.group(2));
+      int patch = Optional.ofNullable(versionMatcher.group(4)).filter(s -> !s.isEmpty()).map(Integer::parseInt).orElse(0);
+      if (major == 1) {
+        return new Version(minor, patch);
+      } else {
+        return new Version(major, minor, patch);
+      }
+    } else {
+      return Validate.getNumber(replaced)
+        .map(BigDecimal::intValue)
+        .map(Version::new)
+        .orElseGet(() -> {
+          MessageUtils.sendMessage(uuid, BetterGUI.getInstance().get(MessageConfig.class).getInvalidNumber(replaced));
+          return new Version(0);
+        });
+    }
   }
 
   @Override
-  protected Result checkConverted(UUID uuid, Integer value) {
-    return VersionUtils.isAtLeast(value) ? Result.success() : Result.fail();
+  protected Result checkConverted(UUID uuid, Version value) {
+    return VersionUtils.compare(value.major, value.minor, value.patch) >= 0 ? Result.success() : Result.fail();
+  }
+
+  public static final class Version {
+    final int major;
+    final int minor;
+    final int patch;
+
+    private Version(int major) {
+      this(major, VersionUtils.getMinorVersion(), VersionUtils.getPatchVersion());
+    }
+
+    private Version(int major, int minor) {
+      this(major, minor, VersionUtils.getPatchVersion());
+    }
+
+    private Version(int major, int minor, int patch) {
+      this.major = major;
+      this.minor = minor;
+      this.patch = patch;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (o == null || getClass() != o.getClass()) return false;
+      Version version = (Version) o;
+      return major == version.major && minor == version.minor && patch == version.patch;
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(major, minor, patch);
+    }
   }
 }
